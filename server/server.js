1802224c83dd1b09a186ecf0583a6d639b3040f3
Ryan Foss1 <Ryan_Foss@anfcorp.com>
2018-07-25 16:22:40 -0400

    update

--- server/server.js
+++ server/server.js
@@ -11,23 +11,15 @@ const hbs = require('hbs');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const express = require('express');
-var app = express();
+const app = express();
 
 // settings
+app.listen(port, () => console.log(`Server is up on ${port}`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(useragent.express());
 hbs.registerPartials(__dirname + '/../views/partials');
 app.set('view engine', 'hbs');
-
-// Maintainence
-// app.use((req, res, next) => {
-// 	res.render('maintainence.hbs', {
-// 		title: 'Site is Down',
-// 		Description: 'Maintainence page',
-// 	});
-// });
-// Setting up /public to not conflict with maintainence
 app.use(express.static(path.join(__dirname, '../views')));
 
 // helpers
@@ -51,15 +43,6 @@ app.get('/weather', (req, res) => {
 		Description: 'Your Weather Report',
 		Keywords: 'Ryan Foss, Weather',
 		css: 'Weather'
-	});
-});
-
-app.get('/contact', (req, res) => {
-	res.render('contact.hbs', {
-		title: 'Contact',
-		Description: 'Contact Ryan Foss for web design or development',
-		Keywords: 'Ryan Foss, Web Development, Web Design, Front End, Back End, HTML, CSS, JavaScript, Express, MongoDB, Node, React',
-		css: 'contact'
 	});
 });
 
@@ -120,7 +103,7 @@ app.get('/KnowYourStuff', function(req, res){
 app.post('/KnowYourStuff', upload.single('file'), (req, res, next) => {
 
 	if(req.file){
-		var results = req.file;
+		const results = req.file;
 		res.render('KnowYourStuff.hbs', {
 			title: 'KnowYourStuff',
 			Description: 'General Tools',
@@ -132,10 +115,9 @@ app.post('/KnowYourStuff', upload.single('file'), (req, res, next) => {
 			size: results.size + 'kb'
 		});
 	} else {
-
-		var language = req.acceptsLanguages();
-		var software = req.useragent.os + ", " + req.useragent.browser;
-		var ipaddress = req.ip;
+		const language = req.acceptsLanguages();
+		const software = req.useragent.os + ", " + req.useragent.browser;
+		const ipaddress = req.ip;
 		if (ipaddress.substr(0, 7) === "::ffff:") {
 	    	ipaddress = ipaddress.substr(7);
 		}
@@ -148,13 +130,5 @@ app.post('/KnowYourStuff', upload.single('file'), (req, res, next) => {
 			software,
 			ipaddress
 		});
-
 	}
-
-});
-
-
-// listener
-app.listen(port, () => {
-  console.log(`Server is up on ${port}`);
 });
