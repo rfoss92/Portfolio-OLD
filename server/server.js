d710c7201e26d1f63f86a6cf6d7b46c2973a770d
Ryan Foss1 <Ryan_Foss@anfcorp.com>
2018-08-15 12:15:25 -0400

    updates

--- server/server.js
+++ server/server.js
@@ -1,12 +1,4 @@
 // config
-require('dotenv').config();
-const http = require('http');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-const cors = require('cors');
-const useragent = require('express-useragent');
 const hbs = require('hbs');
 const path = require('path');
 const port = process.env.PORT || 3000;
@@ -15,9 +7,6 @@ const app = express();
 
 // settings
 app.listen(port, () => console.log(`Server is up on ${port}`));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(useragent.express());
 hbs.registerPartials(__dirname + '/../views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '../views')));
@@ -34,50 +23,5 @@ app.get('/', (req, res) => {
 		description: 'Front-End and Back End Software Developer and Javascript Specialist Portfolio for Ryan Foss',
 		Keywords: 'Ryan Foss, Web Developer, Web Designer, Front End, Back End, HTML, CSS, JavaScript, Express, MongoDB, Node, React',
 		css: 'Index'
-	});
-});
-
-app.get('/Welcome', (req, res) => {
-	res.render('Welcome.hbs', {
-		title: 'Welcome',
-		Description: 'Your Welcome Page',
-		Keywords: 'Ryan Foss, Welcome',
-		css: 'Welcome'
-	});
-});
-
-app.get('/AlanWatts', (req, res) => {
-	res.render('AlanWatts.hbs', {
-		title: 'Alan Watts Tribute',
-		Description: 'A Tribute to Alan Watts',
-		Keywords: 'Ryan Foss, Tribute, Alan Watts, Philosophy',
-		css: 'AlanWatts'
-	});
-});
-
-app.get('/SimonGame', (req, res) => {
-	res.render('SimonGame.hbs', {
-		title: 'Simon Game',
-		Description: 'A Simon Game using Javascript',
-		Keywords: 'Ryan Foss, Simon Game',
-		css: 'SimonGame'
-	});
-});
-
-app.get('/Pomodoro', (req, res) => {
-	res.render('Pomodoro.hbs', {
-		title: 'Pomodoro',
-		Description: 'A Pomodoro clock for productivity',
-		Keywords: 'Ryan Foss, Pomodoro, Clock, Productivity',
-		css: 'Pomodoro'
-	});
-});
-
-app.get('/Calculator', (req, res) => {
-	res.render('Calculator.hbs', {
-		title: 'Calculator',
-		Description: 'A Calculator',
-		Keywords: 'Ryan Foss, Calculator',
-		css: 'Calculator'
 	});
 });
