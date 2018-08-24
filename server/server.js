dcb1ed858fcd4cf4d96d3bc0593dd8491ff4ee85
Ryan Foss1 <Ryan_Foss@anfcorp.com>
2018-08-24 11:39:31 -0400

    updates

--- server/server.js
+++ server/server.js
@@ -18,13 +18,13 @@ hbs.registerHelper('getCurrentYear', () => {
 
 // routes
 app.get('/', (req, res) => {
-	res.redirect('/Web-Developer/');
+	res.redirect(301, '/Web-Developer/');
 });
 
 app.get('/Web-Developer/', (req, res) => {
 	res.render('index.hbs', {
 		title: 'Ryan Foss - Quick, Clean Web Development',
-		description: 'Web Developer that specializing in Javascript to create responsive, clean websites',
+		description: 'Web Developer specializing in Javascript to create responsive, clean websites',
 		Keywords: 'ryanmfoss, Ryan Foss, Web Developer, Web Designer, JavaScript Specialist, Node.js Specialist, React.js Specialist',
 		css: 'Index'
 	});
